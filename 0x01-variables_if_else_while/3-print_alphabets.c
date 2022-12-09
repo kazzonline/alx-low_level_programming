9b3d868147c4c1cb027f87abd83434163478da84
kazzonline <azeezolatunji02@gmail.com>
2022-12-09 14:20:05 -0800

    alphABET  task

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,19 +2,26 @@
 #include <stdlib.h>
 
 /**
- * main - returns alphabets
+ * main - returns alphabets both lower and upper cases
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int ch = 'a';
+	int CH = 'A';
 
 	while (ch <= 'z')
 	{
 		putchar(ch);
 		ch++;
 	}
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+
 	putchar('\n');
 	return (0);
 }
