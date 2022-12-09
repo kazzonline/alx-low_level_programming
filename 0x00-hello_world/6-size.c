9fe8516d4636f89b2a2ef293856b48e80fba6ffc
kazzonline <azeezolatunji02@gmail.com>
2022-12-09 11:08:05 -0800

    My Re-Re-Size is not grandeur, and territory does not make a nation task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,11 @@ int main(void)
 {
 
 	printf("size of a char: %lu byte(s)\n", sizeof(char));
-	pritnf("size of an int: %lu byte(s)\n", sizeof(int));
-	pritnf("size of a long int: %lu byte(s)\n", sizeof(long int));
-	pritnf("size of a long long int: %lu byte(s)\n" sizeof(long long int));
-	printf("size of a float: %lu byte(s)\n" sizeof(float));
+	printf("size of an int: %lu byte(s)\n", sizeof(int));
+	printf("size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("size of a float: %lu byte(s)\n", sizeof(float));
 
-	return (0);
+	return(0);
 
 }
