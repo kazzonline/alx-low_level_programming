225c20ba02356a98348759624cb21e8983eef030
kazzonline <azeezolatunji02@gmail.com>
2023-02-10 12:22:21 -0800

    100-elf_header.c task

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -48,7 +48,7 @@ void check_elf(unsigned char *e_ident)
 /**
  * print_magic - Prints the magic numbers of an ELF header.
  * @e_ident: A pointer to an array containing the ELF magic numbers.
- * 
+ *
  * Description: Magic numbers are separated by spaces.
  */
 void print_magic(unsigned char *e_ident)
